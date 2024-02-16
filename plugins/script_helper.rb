bd2203ccad9e8f76948df15a93a8b01663c2355a
Tom de Bruijn <tom@tomdebruijn.com>
2024-02-16 11:54:20 +0100

    Initial commit for May 2024 workshop
    Copy of the May 2023 workshop page, but updated for the next event.

---
+++ plugins/script_helper.rb
@@ -0,0 +1,31 @@
+require "digest"
+
+module ScriptHelper
+  def csp_policy
+    script_src =
+      if Bridgetown.env.development?
+        "'unsafe-inline'"
+      else
+        "'sha256-#{sha256_for plausible_script}' 'sha256-#{sha256_for plausible_404_script}'"
+      end
+    raw "default-src 'none'; script-src 'self' https://plausible.io #{script_src}; connect-src 'self' https://plausible.io; frame-src https://www.google.com; img-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self'; base-uri 'self'; form-action 'self'"
+  end
+
+  def plausible_domain
+    site.metadata.plausible_domain
+  end
+
+  def plausible_script
+    "window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }"
+  end
+
+  def plausible_404_script
+    %(plausible("404",{ props: { path: document.location.pathname } });)
+  end
+
+  def sha256_for(script)
+    Digest::SHA256.base64digest(script)
+  end
+end
+
+Bridgetown::RubyTemplateView::Helpers.include ScriptHelper
